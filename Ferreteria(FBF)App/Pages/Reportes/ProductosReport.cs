4c55143ba21098a962e582aced5f257fd4e57198
Oliver Jose Fermin Valdez <oliver_fermin@ucne.edu.do>
2020-07-14 13:33:54 -0400

    Reporting

---
+++ Ferreteria(FBF)App/Pages/Reportes/ProductosReport.cs
@@ -0,0 +1,286 @@
+﻿using Ferreteria_FBF_App.BLL;
+using Ferreteria_FBF_App.Models;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Parcial1_Ap2_Fermin.Pages.Reportes
+{
+    public class ProductosReport
+    {
+        #region Declaration
+        int maxColumn = 6;
+        Document document;
+        PdfPTable pdfPTable = new PdfPTable(6);
+        PdfPCell pdfCell;
+        Font fontStyle;
+        Font fontFecha;
+        Font Titulo;
+        MemoryStream memoryStream = new MemoryStream();
+        List<Productos> lista = new List<Productos>();
+        #endregion
+
+        public byte[] Report(List<Productos> Productos)
+        {
+            lista = Productos;
+
+            document = new Document(PageSize.A4, 25f, 25f, 20f, 30f);
+            pdfPTable.WidthPercentage = 100;
+            pdfPTable.HorizontalAlignment = Element.ALIGN_LEFT;
+            fontStyle = FontFactory.GetFont("Calibri", 8f, 1);
+            PdfWriter.GetInstance(document, memoryStream);
+            document.Open();
+
+            float[] sizes = new float[maxColumn];
+
+            for (int i = 0; i < maxColumn; i++)
+            {
+                if (i == 0) sizes[i] = 50;
+                else sizes[i] = 100;
+
+            }
+
+            pdfPTable.SetWidths(sizes);
+
+            this.ReportHeader();
+            this.ReportBody();
+
+            pdfPTable.HeaderRows = 6;
+            document.Add(pdfPTable);
+            document.Close();
+
+            return memoryStream.ToArray();
+        }
+
+        private void ReportHeader()
+        {
+            pdfCell = new PdfPCell(this.AddLogo());
+            pdfCell.Colspan = 1;
+            pdfCell.Border = 0;
+            pdfPTable.AddCell(pdfCell);
+
+
+            pdfCell = new PdfPCell(this.setPageTitle());
+            pdfCell.Colspan = maxColumn;
+            pdfCell.Border = 0;
+            pdfPTable.AddCell(pdfCell);
+
+            pdfPTable.CompleteRow();
+
+        }
+        private PdfPTable AddLogo()
+        {
+            int maxColumn = 1;
+            PdfPTable pdfPTable = new PdfPTable(maxColumn);
+            string img = $"{Directory.GetCurrentDirectory()}{@"\wwwroot\Resources\LogoReport2.png"}";
+            Image image = Image.GetInstance(img);
+
+            pdfCell = new PdfPCell(image);
+            pdfCell.Colspan = maxColumn;
+            pdfCell.HorizontalAlignment = Element.ALIGN_LEFT;
+            pdfCell.Border = 0;
+            pdfCell.ExtraParagraphSpace = 0;
+            pdfPTable.AddCell(pdfCell);
+
+            pdfPTable.CompleteRow();
+
+            return pdfPTable;
+        }
+
+        private PdfPTable setPageTitle()
+        {
+            int maxColumn = 2;
+            PdfPTable pdfPTable = new PdfPTable(maxColumn);
+
+            fontStyle = FontFactory.GetFont("Calibri", 18f, 1);
+            fontFecha = FontFactory.GetFont("Calibri", 10f, 1);
+            Titulo = FontFactory.GetFont("Calibri", 25f, 1);
+
+            pdfCell = new PdfPCell(new Phrase("Ferreteria FBF", Titulo));
+            pdfCell.Colspan = maxColumn;
+            pdfCell.HorizontalAlignment = Element.ALIGN_CENTER;
+            pdfCell.Border = 0;
+            pdfCell.ExtraParagraphSpace = 0;
+            pdfPTable.AddCell(pdfCell);
+
+            pdfPTable.CompleteRow();
+
+            pdfCell = new PdfPCell(new Phrase("Reporte de Articulos", fontStyle));
+            pdfCell.Colspan = maxColumn;
+            pdfCell.HorizontalAlignment = Element.ALIGN_CENTER;
+            pdfCell.Border = 0;
+            pdfCell.ExtraParagraphSpace = 0;
+            pdfPTable.AddCell(pdfCell);
+
+            pdfPTable.CompleteRow();
+
+            pdfCell = new PdfPCell(new Phrase(DateTime.Now.ToString("MM/dd/yyyy H:mm tt"), fontFecha));
+            pdfCell.Colspan = maxColumn;
+            pdfCell.HorizontalAlignment = Element.ALIGN_CENTER;
+            pdfCell.Border = 0;
+            pdfCell.ExtraParagraphSpace = 0;
+            pdfPTable.AddCell(pdfCell);
+
+            pdfPTable.CompleteRow();
+
+            pdfCell = new PdfPCell(new Phrase(" ", fontStyle));
+            pdfCell.Colspan = maxColumn;
+            pdfCell.HorizontalAlignment = Element.ALIGN_CENTER;
+            pdfCell.Border = 0;
+            pdfCell.ExtraParagraphSpace = 0;
+            pdfPTable.AddCell(pdfCell);
+
+            pdfPTable.CompleteRow();
+
+            return pdfPTable;
+        }
+        private void ReportBody()
+        {
+            fontStyle = FontFactory.GetFont("Calibri", 9f, 1);
+            var _fontStyle = FontFactory.GetFont("Calibri", 9f, 0);
+
+            #region Table Header
+            pdfCell = new PdfPCell(new Phrase("ID", fontStyle));
+            pdfCell.HorizontalAlignment = Element.ALIGN_CENTER;
+            pdfCell.VerticalAlignment = Element.ALIGN_MIDDLE;
+            pdfCell.BackgroundColor = BaseColor.LightGray;
+            pdfPTable.AddCell(pdfCell);
+
+            pdfCell = new PdfPCell(new Phrase("Descripcion", fontStyle));
+            pdfCell.HorizontalAlignment = Element.ALIGN_CENTER;
+            pdfCell.VerticalAlignment = Element.ALIGN_MIDDLE;
+            pdfCell.BackgroundColor = BaseColor.LightGray;
+            pdfPTable.AddCell(pdfCell);
+
+
+            pdfCell = new PdfPCell(new Phrase("Marca", fontStyle));
+            pdfCell.HorizontalAlignment = Element.ALIGN_CENTER;
+            pdfCell.VerticalAlignment = Element.ALIGN_MIDDLE;
+            pdfCell.BackgroundColor = BaseColor.LightGray;
+            pdfPTable.AddCell(pdfCell);
+
+
+            pdfCell = new PdfPCell(new Phrase("Unidad", fontStyle));
+            pdfCell.HorizontalAlignment = Element.ALIGN_CENTER;
+            pdfCell.VerticalAlignment = Element.ALIGN_MIDDLE;
+            pdfCell.BackgroundColor = BaseColor.LightGray;
+            pdfPTable.AddCell(pdfCell);
+
+
+            pdfCell = new PdfPCell(new Phrase("Precio unitario", fontStyle));
+            pdfCell.HorizontalAlignment = Element.ALIGN_CENTER;
+            pdfCell.VerticalAlignment = Element.ALIGN_MIDDLE;
+            pdfCell.BackgroundColor = BaseColor.LightGray;
+            pdfPTable.AddCell(pdfCell);
+
+            pdfCell = new PdfPCell(new Phrase("Valor inventario", fontStyle));
+            pdfCell.HorizontalAlignment = Element.ALIGN_CENTER;
+            pdfCell.VerticalAlignment = Element.ALIGN_MIDDLE;
+            pdfCell.BackgroundColor = BaseColor.LightGray;
+            pdfPTable.AddCell(pdfCell);
+
+            pdfPTable.CompleteRow();
+            #endregion
+
+            #region Table Body
+            int num = 0;
+            double inventario = 0;
+            foreach (var item in lista)
+            {
+                inventario += item.ValorInventario;
+                num++;
+                pdfCell = new PdfPCell(new Phrase(item.ProductoId.ToString(), _fontStyle));
+                pdfCell.HorizontalAlignment = Element.ALIGN_CENTER;
+                pdfCell.VerticalAlignment = Element.ALIGN_MIDDLE;
+                pdfCell.BackgroundColor = BaseColor.White;
+                pdfPTable.AddCell(pdfCell);
+
+                pdfCell = new PdfPCell(new Phrase(item.Descripción, _fontStyle));
+                pdfCell.HorizontalAlignment = Element.ALIGN_CENTER;
+                pdfCell.VerticalAlignment = Element.ALIGN_MIDDLE;
+                pdfCell.BackgroundColor = BaseColor.White;
+                pdfPTable.AddCell(pdfCell);
+
+
+                pdfCell = new PdfPCell(new Phrase(MarcasBLL.Buscar(item.MarcaId).Descripcion.ToString(), _fontStyle));
+                pdfCell.HorizontalAlignment = Element.ALIGN_CENTER;
+                pdfCell.VerticalAlignment = Element.ALIGN_MIDDLE;
+                pdfCell.BackgroundColor = BaseColor.White;
+                pdfPTable.AddCell(pdfCell);
+
+
+                pdfCell = new PdfPCell(new Phrase(item.Unidad, _fontStyle));
+                pdfCell.HorizontalAlignment = Element.ALIGN_CENTER;
+                pdfCell.VerticalAlignment = Element.ALIGN_MIDDLE;
+                pdfCell.BackgroundColor = BaseColor.White;
+                pdfPTable.AddCell(pdfCell);
+
+                pdfCell = new PdfPCell(new Phrase(item.PrecioUnitario.ToString(), _fontStyle));
+                pdfCell.HorizontalAlignment = Element.ALIGN_CENTER;
+                pdfCell.VerticalAlignment = Element.ALIGN_MIDDLE;
+                pdfCell.BackgroundColor = BaseColor.White;
+                pdfPTable.AddCell(pdfCell);
+
+                pdfCell = new PdfPCell(new Phrase(item.ValorInventario.ToString(), _fontStyle));
+                pdfCell.HorizontalAlignment = Element.ALIGN_CENTER;
+                pdfCell.VerticalAlignment = Element.ALIGN_MIDDLE;
+                pdfCell.BackgroundColor = BaseColor.White;
+                pdfPTable.AddCell(pdfCell);
+
+                pdfPTable.CompleteRow();
+
+            }
+
+            pdfCell = new PdfPCell(new Phrase(num++.ToString(), fontStyle));
+            pdfCell.HorizontalAlignment = Element.ALIGN_CENTER;
+            pdfCell.VerticalAlignment = Element.ALIGN_MIDDLE;
+            pdfCell.BackgroundColor = BaseColor.White;
+            pdfCell.Border = 0;
+            pdfPTable.AddCell(pdfCell);
+
+            pdfCell = new PdfPCell(new Phrase(" ", fontStyle));
+            pdfCell.HorizontalAlignment = Element.ALIGN_CENTER;
+            pdfCell.VerticalAlignment = Element.ALIGN_MIDDLE;
+            pdfCell.BackgroundColor = BaseColor.White;
+            pdfCell.Border = 0;
+            pdfPTable.AddCell(pdfCell);
+
+            pdfCell = new PdfPCell(new Phrase(" ", fontStyle));
+            pdfCell.HorizontalAlignment = Element.ALIGN_CENTER;
+            pdfCell.VerticalAlignment = Element.ALIGN_MIDDLE;
+            pdfCell.BackgroundColor = BaseColor.White;
+            pdfCell.Border = 0;
+            pdfPTable.AddCell(pdfCell);
+
+            pdfCell = new PdfPCell(new Phrase(" ", fontStyle));
+            pdfCell.HorizontalAlignment = Element.ALIGN_CENTER;
+            pdfCell.VerticalAlignment = Element.ALIGN_MIDDLE;
+            pdfCell.BackgroundColor = BaseColor.White;
+            pdfCell.Border = 0;
+            pdfPTable.AddCell(pdfCell);
+
+            pdfCell = new PdfPCell(new Phrase(" ", fontStyle));
+            pdfCell.HorizontalAlignment = Element.ALIGN_CENTER;
+            pdfCell.VerticalAlignment = Element.ALIGN_MIDDLE;
+            pdfCell.BackgroundColor = BaseColor.White;
+            pdfCell.Border = 0;
+            pdfPTable.AddCell(pdfCell);
+
+            pdfCell = new PdfPCell(new Phrase(inventario.ToString(), fontStyle));
+            pdfCell.HorizontalAlignment = Element.ALIGN_CENTER;
+            pdfCell.VerticalAlignment = Element.ALIGN_MIDDLE;
+            pdfCell.BackgroundColor = BaseColor.White;
+            pdfCell.Border = 0;
+
+            pdfPTable.AddCell(pdfCell);
+
+            pdfPTable.CompleteRow();
+            #endregion
+        }
+
+    }
+}
